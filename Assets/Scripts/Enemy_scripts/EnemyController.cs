5ca44eaf1faab758713c9b62516b28087605e543
Pox1l <suche06@gmail.com>
2024-12-01 20:11:32 +0100

    Enemy anim.
    Enemy rotate anim.

--- Assets/Scripts/Enemy_scripts/EnemyController.cs
+++ Assets/Scripts/Enemy_scripts/EnemyController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public Transform player;
     private Transform target;
     public float speed;
- 
+    private Animator animator;
 
     void Start()
     {
-      target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     void Update()
     {
+        Vector2 direction = (player.position - transform.position).normalized;
+        float speed = direction.magnitude;
+
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed* Time.deltaTime);
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", speed);
     }
 
 
